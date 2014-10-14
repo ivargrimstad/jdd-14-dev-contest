1b0a5cfdaee7532c1fc052a8c3d51e98fb579bc0
Ivar Grimstad <ivar.grimstad@gmail.com>
2014-10-14 08:28:10 +0200

    First test passed
    changed salary test passed (old school)
    
    all tests but deep structure passes (old school)
    
    converted to lambdas. still not supporting deep structures
    
    fixed last test

--- src/main/java/pl/allegro/jdd/StructureDiff.java
+++ src/main/java/pl/allegro/jdd/StructureDiff.java
@@ -1,13 +1,44 @@
 package pl.allegro.jdd;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import com.google.common.collect.TreeTraverser;
+import static java.util.stream.Collectors.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class StructureDiff {
-    public Changes calculate(Employee oldCTO, Employee newCTO){
-        checkNotNull(oldCTO);
-        checkNotNull(newCTO);
 
-        // your code goes here
-        throw new UnsupportedOperationException("Please implement me");
-    }
+   public Changes calculate(Employee oldCTO, Employee newCTO) {
+      checkNotNull(oldCTO);
+      checkNotNull(newCTO);
+
+      TreeTraverser<Employee> treeTraverser = new TreeTraverser<Employee>() {
+         @Override
+         public Iterable<Employee> children(Employee employee) {
+            return employee.getSubordinates();
+         }
+      };
+
+      final Set<Employee> oldies = treeTraverser.breadthFirstTraversal(oldCTO).toSet();
+      final Set<Employee> newbies = treeTraverser.breadthFirstTraversal(newCTO).toSet();
+
+      final List<Employee> fired = oldies.stream()
+              .filter(oldE -> !newCTO.getSubordinates().contains(oldE) && !oldE.equals(newCTO))
+              .collect(toList());
+
+      final List<Employee> changedSalary = new ArrayList<>();
+      oldies.stream()
+              .forEach(o -> {
+                 newbies.stream().filter(n -> o.getSalary() != n.getSalary()).forEach(i -> {
+                    changedSalary.add(o);
+                 });
+              });
+
+      final List<Employee> hired = newbies.stream()
+              .filter(newE -> !oldies.contains(newE))
+              .collect(toList());
+
+      return new Changes(fired, hired, changedSalary);
+   }
 }
